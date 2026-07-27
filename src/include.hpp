24db2a20ad01cb5df364d8518777eabbadee21a9
Trường Chinh <chinhcuber@gmail.com>
2026-07-27 11:57:15 +0700

    Delete src directory

--- src/include.hpp
+++
@@ -1,6 +0,0 @@
-#include "lib//header.hpp"
-#include "lib//user_blockchain.hpp"
-#include "lib//transaction_manager.hpp"
-#include "lib//blockchain.hpp"
-#include "lib//p2p.hpp"
-#include "lib//color.h"
